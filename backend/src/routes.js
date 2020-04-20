3cc893a1d213edc2b8dd73067cfca83f82d4e39b
Gabriel Soares <gabrielsoaresii@outlook.com>
2020-04-20 15:27:43 -0300

    Adding profileController

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const ongController = require('./controllers/OngController')
 const incidentController = require('./controllers/incidentController')
+const profileController = require('./controllers/profileController')
 
 const routes = express.Router();
 
 routes.post("/ongs", ongController.create)
 routes.get("/ongs", ongController.index)
+
+routes.get("/profile", profileController.index)
 
 routes.post("/incidents", incidentController.create)
 routes.get("/incidents", incidentController.index)
