a8d9b90de5aab53ad0bdc9d373e84b095a1a33f7
Gabriel Soares <gabrielsoaresii@outlook.com>
2020-04-26 20:20:29 -0300

    Adding Logon, Profile and Register pages

--- frontend/src/pages/Logon/index.js
+++ frontend/src/pages/Logon/index.js
@@ -1,8 +1,34 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './styles.css';
+
+import {FiLogIn} from 'react-icons/fi'
+
+import logoimg from '../../assets/logo.svg'
+import heroesimg from '../../assets/heroes.png'
 
 export default function Logon() {
   return (
-    <h1>Hello World</h1>
+    <div>
+      <div className="logon-container">
+        <section className="form">
+          <img src={logoimg} alt="Be The Hero"/>
+          
+          <form>
+            <h1>Faça seu logon</h1>
+
+            <input placeholder="Sua ID"/>
+            <button className="button" type="submit">Entrar</button>
+
+            <Link className="back-link" to="/register">
+              <FiLogIn size={16} color="#E02041"/>
+              Não tenho cadastro  
+            </Link>
+          </form>
+        </section>
+
+        <img src={heroesimg} alt="Heroes"/>  
+      </div>
+    </div>
   )
 }
