217970209170c8893ac4cdafab0662d46b7b68aa
Gabriel Soares <gabrielsoaresii@outlook.com>
2020-05-10 01:16:29 -0300

    Adding Logon functionality

--- frontend/src/pages/Logon/index.js
+++ frontend/src/pages/Logon/index.js
@@ -1,6 +1,8 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link, useHistory } from 'react-router-dom';
 import './styles.css';
+
+import api from   '../../services/api';
 
 import {FiLogIn} from 'react-icons/fi'
 
@@ -8,16 +10,39 @@ import logoimg from '../../assets/logo.svg'
 import heroesimg from '../../assets/heroes.png'
 
 export default function Logon() {
+  const [id, setId] = useState('');
+  const history = useHistory();
+
+  async function handleLogin(e){
+    e.preventDefault();
+
+    try{
+      const response = await api.post('session', { id })
+      
+      localStorage.setItem("ongId", id)
+      localStorage.setItem("ongName", response.data.name)
+      
+      history.push("/profile")
+    }catch(err){
+      alert("Falha no login, tente novamente.")
+    }
+  }
+
   return (
     <div>
       <div className="logon-container">
         <section className="form">
           <img src={logoimg} alt="Be The Hero"/>
           
-          <form>
+          <form onSubmit={handleLogin}>
             <h1>Faça seu logon</h1>
 
-            <input placeholder="Sua ID"/>
+            <input 
+            placeholder="Sua ID"
+            value={id}
+            onChange={e => setId(e.target.value)}
+            />
+
             <button className="button" type="submit">Entrar</button>
 
             <Link className="back-link" to="/register">
--- frontend/src/pages/Profile/index.js
+++ frontend/src/pages/Profile/index.js
@@ -1,17 +1,35 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { FiPower, FiTrash2 } from 'react-icons/fi';
+
+import api from '../../services/api';
 
 import logoimg from '../../assets/logo.svg'
 
 import './styles.css'
 
 export default function Profile() {
+  const [incidents, setIncidents] = useState([]);
+
+  const ongId = localStorage.getItem("ongId")
+  const ongName = localStorage.getItem("ongName");
+  
+  useEffect(() => {
+    api.get('profile', {
+      headers: {
+        Authorization: ongId,
+      }
+    }).then(response => {
+      console.log(response.data)
+      setIncidents(response.data)
+    })
+  }, [ongId]);
+
   return (
     <div className="profile-container">
       <header>
         <img src={logoimg} alt="Be The Hero" />
-        <span>Bem vinda, APAD</span>
+        <span>Bem vinda, {ongName}</span>
 
         <Link className="button" to="/incidents/new">Cadastrar novo caso</Link>
         <button type="button">
